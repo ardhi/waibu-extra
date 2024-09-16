8a5dd5d6744886296614939c3547b9592074deb8
Ardhi Lukianto <ardhi@lukianto.com>
2024-09-16 20:45:08 +0700

    Move alpinejs to its own plugin

--- waibuStatic/virtual.js
+++ waibuStatic/virtual.js
@@ -1,10 +1,10 @@
 async function virtual () {
-  const { readConfig } = this.app.bajo
+  const { readJson } = this.app.bajo
   const { pick } = this.app.bajo.lib._
-  const libs = await readConfig(`${this.name}:/lib/libs.json`)
+  const libs = readJson(`${this.dir.pkg}/lib/libs.json`)
   const virts = []
   for (const l of libs) {
-    if (this.config.virtuals.includes(l.prefix)) virts.push(pick(l, ['prefix', 'root']))
+    if (this.config.scripts.includes(l.prefix)) virts.push(pick(l, ['prefix', 'root']))
   }
   return virts
 }

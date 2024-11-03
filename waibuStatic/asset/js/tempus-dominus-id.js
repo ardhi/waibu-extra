65b7cdc28ea76c5f6115e5d5f724d2aa232db4fb
Ardhi Lukianto <ardhi@lukianto.com>
2024-11-03 11:09:00 +0700

    Updates

---
+++ waibuStatic/asset/js/tempus-dominus-id.js
@@ -0,0 +1,46 @@
+/* global _ */
+
+_.set(window, 'tempusDominus.locales.id', {
+  name: 'id',
+  localization: {
+    today: 'Hari Ini',
+    clear: 'Hapus pilihan',
+    close: 'Tutup kotak pilihan',
+    selectMonth: 'Pilih bulan',
+    previousMonth: 'Bulan sebelumnya',
+    nextMonth: 'Bulan sesudahnya',
+    selectYear: 'Pilih tahun',
+    previousYear: 'Tahun sebelumnya',
+    nextYear: 'Tahun sesudahnya',
+    selectDecade: 'Pilih dekade',
+    previousDecade: 'Dekade sebelumnya',
+    nextDecade: 'Dekade sesudahnya',
+    previousCentury: 'Abad sebelumnya',
+    nextCentury: 'Abad sesudahnya',
+    pickHour: 'Pilih jam',
+    incrementHour: 'Naikkan jam',
+    decrementHour: 'Turunkan jam',
+    pickMinute: 'Pilih menit',
+    incrementMinute: 'Naikkan menit',
+    decrementMinute: 'Turunkan menit',
+    pickSecond: 'Pilih detik',
+    incrementSecond: 'Naikkan detik',
+    decrementSecond: 'Turunkan detik',
+    toggleMeridiem: 'Alihkan tanda waktu',
+    selectTime: 'Pilih waktu',
+    selectDate: 'Pilih tanggal',
+    dayViewHeaderFormat: { month: 'long', year: '2-digit' },
+    locale: 'id',
+    startOfTheWeek: 1,
+    dateFormats: {
+      LTS: 'HH:mm:ss',
+      LT: 'HH:mm',
+      L: 'dd/MM/yyyy',
+      LL: 'd MMMM yyyy',
+      LLL: 'd MMMM yyyy HH:mm',
+      LLLL: 'dddd, d MMMM yyyy HH:mm'
+    },
+    ordinal: (n) => `${n}.`,
+    format: 'L LTS'
+  }
+})

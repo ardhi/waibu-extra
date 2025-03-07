bc3a8b6300ea5add1dd5452258d928dccd6f6b29
Ardhi Lukianto <ardhi@lukianto.com>
2025-03-07 22:45:24 +0700

    Updates

--- waibuStatic/virtual.js
+++ waibuStatic/virtual.js
@@ -1,7 +1,7 @@
 async function virtual () {
   const { readJson } = this.app.bajo
   const { pick } = this.app.bajo.lib._
-  const libs = readJson(`${this.dir.pkg}/lib/libs.json`)
+  const libs = readJson(`${this.dir.pkg}/lib/libs.json`) ?? []
   const virts = []
   for (const l of libs) {
     if (this.config.libs.includes(l.prefix)) virts.push(pick(l, ['prefix', 'root']))

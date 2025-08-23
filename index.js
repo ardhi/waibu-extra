d04d80ef4135e116ad4ebc2ca50d2281818e475f
Ardhi Lukianto <ardhi@lukianto.com>
2025-08-23 23:45:26 +0700

    Updates

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 async function factory (pkgName) {
   const me = this
 
-  return class WaibuExtra extends this.lib.BajoPlugin {
+  return class WaibuExtra extends this.lib.Plugin {
     constructor () {
       super(pkgName, me.app)
       this.alias = 'wx'
